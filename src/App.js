0862d2be31c7a7879bc20372014e19f05682505b
Krupa Ghetia <krupaghetia@Krupas-MacBook-Pro.local>
2021-03-12 03:23:05 +0530

    learn react router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import { StrictMode } from "react";
-import ReactDom from "react-dom";
-
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
 
-ReactDom.render(
+render(
   <StrictMode>
-    <App />{" "}
+    <App />
   </StrictMode>,
   document.getElementById("root")
 );
