b9300a040439baba1306799c7b95def2f137742c
Krupa Ghetia <krupaghetia@Krupas-MacBook-Pro.local>
2021-03-12 05:02:53 +0530

    learn react context

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
-import { withRouter } from "react-router";
-
+import { withRouter } from "react-router-dom";
+import ThemeContext from "./ThemeContext";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 
@@ -11,21 +11,13 @@ class Details extends Component {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
-
     const json = await res.json();
-    this.setState(
-      Object.assign(
-        {
-          loading: false,
-        },
-        json.pets[0]
-      )
-    );
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
   render() {
     if (this.state.loading) {
-      return <h2>Loading...</h2>;
+      return <h2>loading … </h2>;
     }
 
     const {
@@ -43,10 +35,12 @@ class Details extends Component {
         <Carousel images={images} />
         <div>
           <h1>{name}</h1>
-          <h2>
-            {animal} - {breed} - {city}, {state}
-          </h2>
-          <button>Adopt {name}</button>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
@@ -56,7 +50,7 @@ class Details extends Component {
 
 const DetailsWithRouter = withRouter(Details);
 
-export default function DetailsErrorsBoundary(props) {
+export default function DetailsErrorBoundary(props) {
   return (
     <ErrorBoundary>
       <DetailsWithRouter {...props} />
